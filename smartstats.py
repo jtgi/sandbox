1263ee990cd153dbc13ac4c770eee586a1453d25
jtgi <j@jtgi.me>
2017-09-19 22:35:57 -0700

    Implement time travel

--- smartstats.py
+++ smartstats.py
@@ -43,12 +43,15 @@ key_map = {
 }
 
 def start(fighter, round, start_offset):
-    data = {}
-    data['fighter'] = fighter
-    data['round'] = round
-    data['start_offset'] = int(start_offset)
-    data['start_time'] = datetime.datetime.now()
-    data['stats'] = []
+    state = {}
+    state['fighter'] = fighter
+    state['round'] = str(round)
+    state['start_offset'] = int(start_offset)
+    state['start_time'] = datetime.datetime.now()
+    state['stats'] = []
+    state['paused'] = False
+    state['pause_elapsed_seconds'] = 0
+    state['pointer'] = 0
 
     print banner
     print "-----------------------------"
@@ -57,11 +60,25 @@ def start(fighter, round, start_offset):
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
-                onQuit(data)
+                onQuit(state)
+                return
 
             if event.type == KEYDOWN:
                 if event.key == K_q:
-                    onQuit(data)
+                    onQuit(state)
+                    return
+
+                if event.key == K_p:
+                    onTogglePause(state)
+                    break
+
+                if event.key == K_BACKSPACE:
+                    onTimeTravel(state)
+                    break
+
+                if state['paused'] and not timeTraveling(state):
+                    print "Paused (p to resume)"
+                    break
 
                 key = event.unicode
                 pressed_keys = pygame.key.get_pressed()
@@ -74,27 +91,77 @@ def start(fighter, round, start_offset):
 
                 if key in key_map:
                     punch_type = key_map[key]
-                    onKeyPress(data, punch_type)
+                    onKeyPress(state, punch_type)
 
-def onQuit(data):
-    joined = "\n".join(data['stats'])
+def onQuit(state):
+    string_stats = []
+    for stat in state['stats']:
+        string_stats.append(",".join(stat))
+
+    joined = "\n".join(string_stats)
     print joined
 
-    path = "./stats/{}-round-{}.csv".format(data['fighter'], data['round'])
+    path = "./stats/{}-round-{}.csv".format(state['fighter'], state['round'])
     fo = open(path, "a")
     fo.write(joined)
 
     pygame.quit()
     exit()
 
-def onKeyPress(data, punch_type):
-    now = datetime.datetime.now()
-    diff = str(int(round((now - data['start_time']).total_seconds())))
+def onTogglePause(state):
+    if state['paused']:
+        resume(state)
+    else:
+        pause(state)
 
-    stat = "{},{},{},{},{}".format(diff, data['round'], data['fighter'], punch_type[0], punch_type[1])
-    print stat
+def resume(state):
+    state['paused'] = False
+    elapsed_time_seconds = (datetime.datetime.now() - state['pause_start']).total_seconds()
+    state['pause_elapsed_seconds'] += elapsed_time_seconds
+    print "Resumed"
+    return
 
-    data['stats'].append(stat)
+def pause(state):
+    state['paused'] = True
+    state['pause_start'] = datetime.datetime.now()
+    print "Paused"
+
+def onTimeTravel(state):
+    state['pointer'] = max(state['pointer'] - 1, 0);
+    time_in_past = (len(state['stats'])) - state['pointer'];
+    print "{} {} in the past".format(time_in_past, "punches" if time_in_past > 1 else "punch")
+
+def onTimeTravelForward(state):
+    state['pointer'] += 1
+    time_in_past = (len(state['stats'])) - state['pointer'];
+
+    if time_in_past > 0:
+        print "Rewrote Stat"
+        print "{} {} left to go".format(time_in_past, "punches" if time_in_past > 1 else "punch")
+    else:
+        print "Up to date"
+
+def timeTraveling(state):
+    diff = max(len(state['stats']), 0) - state['pointer']
+    return diff > 0
+
+def onKeyPress(state, punch_type):
+    if timeTraveling(state):
+        diff = state['stats'][state['pointer']][0]
+        stat = [diff, state['round'], state['fighter'], str(punch_type[0]), punch_type[1]]
+        state['stats'][state['pointer']] = stat
+
+        print stat
+        onTimeTravelForward(state)
+    else:
+        now = datetime.datetime.now()
+        raw_diff = (now - state['start_time']).total_seconds()
+        diff = str(int(round(raw_diff - state['pause_elapsed_seconds'])))
+        stat = [diff, state['round'], state['fighter'], str(punch_type[0]), punch_type[1]]
+        state['stats'].append(stat)
+        state['pointer'] += 1
+        print stat
+
 
 def main():
     if len(sys.argv) < 3:
@@ -106,7 +173,7 @@ def main():
     offset = int(sys.argv[3]) if len(sys.argv) >= 4 else 0
 
     pygame.init()
-    screen = pygame.display.set_mode((100, 50))
+    screen = pygame.display.set_mode((200, 200))
 
     start(fighter, round, offset)
 
