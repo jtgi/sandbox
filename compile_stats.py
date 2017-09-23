325ea3dfd29b127052e18696196b447a2333f849
jtgi <j@jtgi.me>
2017-09-22 20:16:45 -0700

    Styles and work rates

--- compile_stats.py
+++ compile_stats.py
@@ -1,4 +1,5 @@
 import sys
+import plotly.plotly as py
 import plotly.graph_objs as go
 
 from os import listdir
@@ -12,21 +13,39 @@ FIGHTER = 2
 LANDED = 3
 PUNCH = 4
 
+darker_colors = [
+    dict(
+        color='#a72b39',
+        line=dict(
+            color='#b4303f',
+            width=0,
+        )
+    ),
+    dict(
+        color='#008ca1',
+        line=dict(
+            color='#018599',
+            width=0,
+        )
+    )
+]
+
+
 colors = [
     dict(
-        color='rgba(226, 63, 81, 1)',
+        color='#e23f51',
         line=dict(
-            color='rgba(246, 73, 101, 1.0)',
+            color='#d33c4d',
             width=0,
         )
     ),
     dict(
-        color='rgba(0, 175, 202, 1)',
+        color='#00afca',
         line=dict(
-            color='rgba(20, 195, 222, 1.0)',
+            color='#02a8c2',
             width=0,
         )
-    ),
+    )
 ]
 
 def group_by_fighter(stats):
@@ -44,12 +63,50 @@ def group_by_fighter(stats):
 
     return fighters
 
-def draw_punch_variation(title, stats):
+def draw_punch_variation(title, stats, rounds, landed_only):
     data = []
     max_y = 0
     i = 0
 
-    punch_type = {
+    grouped_stats = group_by_fighter(stats)
+
+    for fighter, fighter_stats in grouped_stats.items():
+        labels = []
+        values = []
+        punch_type = punch_map()
+
+        for stat in fighter_stats:
+            parts = stat.split(",")
+            punch = parts[PUNCH].title()
+
+            if not landed_only or (landed_only and parts[LANDED] == '1'):
+                punch_type[punch] += 1
+                max_y = max(max_y, punch_type[punch])
+
+        for key, value in punch_type.items():
+            labels.append(key)
+            values.append(value)
+
+        data.append(go.Bar(
+            y=values,
+            x=labels,
+            name=fighter,
+            marker=colors[i % len(colors)]
+        ))
+
+        i = i + 1
+
+    graph_title = pretty_rounds(rounds)
+    graph_title += ' - Landed Punches' if landed_only else ' - Thrown Punches'
+    layout = histogram_layout(graph_title, max_y)
+
+    for i, stat in enumerate(data):
+        fig = go.Figure(data=data, layout=layout)
+        filename = 'punch-variation-{}-{}-{}'.format('landed' if landed_only else 'thrown', i, "-".join(rounds))
+        plot(fig, filename='graphs/{}.html'.format(filename))
+
+def punch_map():
+    return {
         "Jab": 0,
         "Cross": 0,
         "Lead Hook": 0,
@@ -64,43 +121,45 @@ def draw_punch_variation(title, stats):
         "Body Rear Upper": 0,
     }
 
+def draw_punch_pie(title, stats, rounds, landed_only):
+    i = 0
     grouped_stats = group_by_fighter(stats)
 
     for fighter, fighter_stats in grouped_stats.items():
-        labels = []
-        values = []
+        head = 0
+        body = 0
+        data = []
 
         for stat in fighter_stats:
             parts = stat.split(",")
-            punch = parts[PUNCH].title()
+            round = parts[ROUND]
+            punch = parts[PUNCH]
 
-            if punch not in punch_type:
-                punch_type[punch] = 0
+            if not landed_only or (landed_only and parts[LANDED] == '1'):
+                if "body" in punch:
+                    body += 1
+                else:
+                    head +=1
 
-            punch_type[punch] += 1
-            max_y = max(max_y, punch_type[punch])
+        cs = [colors[i % len(colors)]['color'], darker_colors[i % len(colors)]['color']]
 
-        for key, value in punch_type.items():
-            labels.append(key)
-            values.append(value)
-
-        data.append(go.Bar(
-            y=labels,
-            x=values,
-            name=fighter,
-            marker=colors[i % len(colors)]
+        data.append(go.Pie(
+            labels=['Head', 'Body'],
+            values=[head, body],
+            hoverinfo='label+percent',
+            textinfo='label+value+percent',
+            marker=dict(
+                colors=cs
+            )
         ))
 
-        i = i + 1
+        graph_title = "{} - {} - Head vs Body".format(pretty_rounds(rounds), "Landed" if landed_only else "Thrown")
+        fig = go.Figure(data=data, layout=pie_layout(graph_title))
+        plot(fig, filename='graphs/{}-punch-pie-{}-{}.html'.format(fighter, landed_only, "-".join(rounds)))
 
-    layout = histogram_layout(title, max_y)
+        i += 1
 
-    for i, stat in enumerate(data):
-        fig = go.Figure(data=[stat], layout=layout)
-        plot(fig, filename='graphs/punch-variation-{}.html'.format(i))
-
-
-def draw_punch_timing(title, stats):
+def draw_punch_timing(title, stats, rounds):
     stacked_bar_data = []
     max_y = 0
     i = 0
@@ -117,12 +176,16 @@ def draw_punch_timing(title, stats):
             parts = stat.split(",")
             secs = int(parts[TIME])
             name = parts[FIGHTER]
+
             if secs not in actions:
                 actions[secs] = [0, []]
 
             actions[secs][1].append(secs)
-            actions[secs][0] = actions[secs][0] + 1
 
+            if i % 2 == 0:
+                actions[secs][0] += 1
+            else:
+                actions[secs][0] -= 1
 
         for key, value in actions.items():
             ys[key] = value[0]
@@ -136,8 +199,18 @@ def draw_punch_timing(title, stats):
 
         i = i + 1
 
-    layout = go.Layout(
-            barmode='stack',
+        graph_title = "{} - Punch Output".format(pretty_rounds(rounds))
+
+        if title:
+            graph_title + ' - ' + title
+
+        layout = bar_layout(graph_title, 'Punch Time (seconds)', 'Punch Count')
+        fig = go.Figure(data=stacked_bar_data, layout=layout)
+        plot(fig, filename='graphs/punch-timing-{}.html'.format("-".join(rounds)))
+
+def bar_layout(title, x_label, y_label):
+    return go.Layout(
+            barmode='relative',
             paper_bgcolor='rgba(35, 35, 35, 1)',
             plot_bgcolor='rgba(35, 35, 35, 1)',
             title=title,
@@ -152,7 +225,7 @@ def draw_punch_timing(title, stats):
                 color='rgb(255, 255, 255)'
             ),
             xaxis=dict(
-                        title='PUNCH TIME (SECONDS)',
+                        title=x_label,
                         titlefont=dict(
                                         size=18,
                                         family='Avenir, sans-serif',
@@ -165,7 +238,7 @@ def draw_punch_timing(title, stats):
                                     )
                     ),
             yaxis=dict(
-                        title='PUNCHES THROWN',
+                        title=y_label,
                         titlefont=dict(
                                         size=18,
                                         family='Avenir, sans-serif',
@@ -184,7 +257,7 @@ def draw_punch_timing(title, stats):
                             color='#FFF'
                         ),
                         x=0,
-                        y=1.0,
+                        y=1.25,
                         bgcolor='rgba(255, 255, 255, 0)',
                         bordercolor='rgba(255, 255, 255, 0)'
                     ),
@@ -192,11 +265,18 @@ def draw_punch_timing(title, stats):
             bargroupgap=0.1
     )
 
-    fig = go.Figure(data=stacked_bar_data, layout=layout)
-    plot(fig, filename='graphs/punch-timing.html')
+def work_rate(stats, rounds):
+    grouped_stats = group_by_fighter(stats)
+
+    for fighter, fighter_stats in grouped_stats.items():
+        round_rate = len(fighter_stats) / len(rounds)
+        second_rate = len(fighter_stats) / (len(rounds) * 180)
+        print "{} {}ppr {}pps".format(fighter, round_rate, second_rate)
 
 def histogram_layout(title, max_y):
-    layout2 = go.Layout(
+    return go.Layout(
+        width=1680,
+        height=1050,
         paper_bgcolor='rgba(35, 35, 35, 1)',
         plot_bgcolor='rgba(35, 35, 35, 1)',
         title=title,
@@ -238,20 +318,68 @@ def histogram_layout(title, max_y):
         ),
         legend=dict(
             font=dict(
-                family='Avenir, sans-serif',
+                family='Avenir Heavy, sans-serif',
                 size=32,
                 color='#FFF'
             ),
             x=0,
-            y=1.0,
+            y=1.25,
             bgcolor='rgba(255, 255, 255, 0)',
             bordercolor='rgba(255, 255, 255, 0)'
         )
     )
 
+def pie_layout(title):
+    return go.Layout(
+            paper_bgcolor='rgba(35, 35, 35, 1)',
+            plot_bgcolor='rgba(35, 35, 35, 1)',
+            title=title,
+            margin=dict(
+                t=300,
+                l=100,
+                r=100,
+                b=200),
+            font=dict(
+                size=32,
+                family='Avenir Heavy, sans-serif',
+                color='rgb(255, 255, 255)'
+            ),
+            legend=dict(
+                        font=dict(
+                            family='Avenir Heavy, sans-serif',
+                            size=32,
+                            color='#FFF'
+                        ),
+                        x=0,
+                        y=1.25,
+                        bgcolor='rgba(255, 255, 255, 0)',
+                        bordercolor='rgba(255, 255, 255, 0)'
+                    )
+    )
+
+def get_rounds(stats):
+    rounds = set()
+
+    grouped_stats = group_by_fighter(stats)
+
+    for fighter, fighter_stats in grouped_stats.items():
+        for stat in fighter_stats:
+            parts = stat.split(",")
+            rounds.add(parts[ROUND])
+
+    return rounds
+
+def pretty_rounds(rounds):
+    if len(rounds) == 1:
+        return "Round {}".format("".join(rounds))
+    elif len(rounds) == 12:
+        return "All Rounds"
+    else:
+        return "Rounds {}".format(", ".join(rounds))
+
 def main():
-    title = sys.argv[1]
     data = []
+    title = sys.argv[1]
 
     if len(sys.argv) == 3:
         dir = sys.argv[2]
@@ -264,8 +392,13 @@ def main():
             lines = f.readlines()
             data.append([x.strip() for x in lines])
 
-    draw_punch_timing(title, data)
-    draw_punch_variation(title, data)
+    rounds = get_rounds(data)
+    draw_punch_timing(title, data, rounds)
+    draw_punch_variation(title, data, rounds, False)
+    draw_punch_variation(title, data, rounds, True)
+    draw_punch_pie(title, data, rounds, False)
+    draw_punch_pie(title, data, rounds, True)
+    work_rate(data, rounds)
 
 if __name__ == "__main__":
       main()
